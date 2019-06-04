239f248f8514909f3c60c792f290510b4fd762ad
PhilipSteinkellner <p.steinkellner@hotmail.com>
2019-06-04 21:43:30 +0200

    finalised task 3 queens problem for size 4

--- src/Task3.java
+++ src/Task3.java
@@ -1,5 +1,3 @@
-import org.jacop.constraints.Alldifferent;
-import org.jacop.constraints.Max;
 import org.jacop.constraints.Sum;
 import org.jacop.core.IntVar;
 import org.jacop.core.Store;
@@ -7,122 +5,105 @@ import org.jacop.search.*;
 
 public class Task3 {
 
-    static Task3 m = new Task3();
-
     public static void main(String[] args) {
-
-        Allsolution allsolution = new Allsolution();
-
 
         Store store = new Store(); // define FD store
         int size = 4;
         int fields = size*size;
 
-
         // define finite domain variables
-
-
         IntVar[] v = new IntVar[fields];
         for (int i = 0; i < fields; i++)
-            v[i] = new IntVar(store, "v" + i, 0, 1); // define constraints
+            v[i] = new IntVar(store, "v" + i, 0, 1);
 
-        //reihe
+        //constraints
+
+        //row
         for (int i = 0; i < fields; i = i + 4) {
-
-            IntVar[] vtmp = {v[i], v[i + 1], v[i + 2], v[i + 3],};
-            IntVar sum = new IntVar(store, "sum",1,1);
+            IntVar[] vtmp = {v[i], v[i + 1], v[i + 2], v[i + 3]};
+            IntVar sum = new IntVar(store, "sum",0,1);
             store.impose(new Sum(vtmp,sum));
         }
 
 
-        //splate
+        //column
         for (int i = 0; i < size; i++) {
             IntVar[] vtmp = {v[i],v[i+4],v[i+8],v[i+12],};
-            IntVar sum = new IntVar(store, "sum",1,1);
+            IntVar sum = new IntVar(store, "sum",0,1);
             store.impose(new Sum(vtmp,sum));
         }
 
 
-        //in summe 4
-        IntVar sum = new IntVar(store, "sum",4,4);
-        store.impose(new Sum(v,sum));
+        //max amount of queens
+        IntVar sumGlobal = new IntVar(store, "sumGlobal",size,size);
+        store.impose(new Sum(v,sumGlobal));
 
-        //max 1
-        IntVar max = new IntVar(store, "max",0,1);
-
-        // rechts ausgerichtet
-
-        /*
+        // diagonal rechts
 
         IntVar[] vtmpr = {v[1],v[4]};
-        store.impose(new Max(vtmpr,max));
+        IntVar sum = new IntVar(store, "sum",0,1);
+        store.impose(new Sum(vtmpr,sum));
 
         IntVar[] vtmpr2 = {v[2],v[5], v[8]};
-        store.impose(new Max(vtmpr2,max));
+        IntVar sum2 = new IntVar(store, "sum",0,1);
+        store.impose(new Sum(vtmpr2,sum2));
 
         IntVar[] vtmpr3 = {v[3],v[6], v[9], v[12]};
-        store.impose(new Max(vtmpr3,max));
+        IntVar sum3 = new IntVar(store, "sum",0,1);
+        store.impose(new Sum(vtmpr3,sum3));
 
         IntVar[] vtmpr4 = {v[7],v[10], v[13]};
-        store.impose(new Max(vtmpr4,max));
+        IntVar sum4 = new IntVar(store, "sum",0,1);
+         store.impose(new Sum(vtmpr4,sum4));
 
         IntVar[] vtmpr5 = {v[11],v[14]};
-        store.impose(new Max(vtmpr5,max));
+        IntVar sum5 = new IntVar(store, "sum",0,1);
+        store.impose(new Sum(vtmpr5,sum5));
 
         //*****************************
 
-        // links ausgerichtet
+        // diagonal links
 
         IntVar[] vtmpl = {v[8],v[13]};
-        store.impose(new Max(vtmpl,max));
+        IntVar sum6 = new IntVar(store, "sum",0,1);
+        store.impose(new Sum(vtmpl,sum6));
 
         IntVar[] vtmpl2 = {v[4],v[9], v[14]};
-        store.impose(new Max(vtmpl2,max));
+        IntVar sum7 = new IntVar(store, "sum",0,1);
+        store.impose(new Sum(vtmpl2,sum7));
 
         IntVar[] vtmpl3 = {v[0],v[5], v[10], v[15]};
-        store.impose(new Max(vtmpl3,max));
+        IntVar sum8 = new IntVar(store, "sum",0,1);
+        store.impose(new Sum(vtmpl3,sum8));
 
         IntVar[] vtmpl4 = {v[1],v[6], v[11]};
-        store.impose(new Max(vtmpl4,max));
+        IntVar sum9 = new IntVar(store, "sum",0,1);
+        store.impose(new Sum(vtmpl4,sum9));
 
         IntVar[] vtmpl5 = {v[2],v[7]};
-        store.impose(new Max(vtmpl5,max));
-
-
-        */
-
+        IntVar sum10 = new IntVar(store, "sum",0,1);
+        store.impose(new Sum(vtmpl5,sum10));
 
 
         // search for a solution and print results
-        Search<IntVar> search = new DepthFirstSearch<IntVar>();
+        Search<IntVar> label = new DepthFirstSearch<IntVar>();
         SelectChoicePoint<IntVar> select =
-                new InputOrderSelect<IntVar>(store, v,
+                new SimpleSelect<IntVar>(v,
+                        new SmallestDomain<IntVar>(),
                         new IndomainMin<IntVar>());
 
-        boolean result = search.labeling(store, select);
+        boolean result = label.labeling(store, select);
 
-        //System.out.println("HUGO" + search.getSolution());
-
-
-        if (result)
-
+        if(result){
             for (int i = 0; i < fields; i++) {
-
                 if (i % 4 == 0) {
                     System.out.println("");
                     System.out.println("------------------");
                 }
-
                 System.out.print(" " + v[i].value() + " | ");
-
             }
-
-
-        else {
-
+        }else {
             System.out.println("*** No");
         }
-
-
     }
 }
